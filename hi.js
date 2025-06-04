e317ac7a563148a48955a754dd9baf4b05235721
joe-2707 <ntnimeshan@gmail.com>
2025-06-04 22:01:28 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,30 @@
+
+var o=document.getElementById(1)
+
+var n1=0
+var n2=0
+var s1=0
+var n3=10
+var nn=0
+
+function number(x)
+{
+    n1=x
+
+}
+
+function sinbl()
+{
+    n2=n1
+    n1=0
+}
+
+function cul()
+{
+   n3=n1*n2
+   o.textContent=n3
+}
+console.log(y)
+console.log(n1)
+console.log(n2)
+console.log(n3)
